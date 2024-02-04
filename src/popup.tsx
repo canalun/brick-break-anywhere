01147be7d847fa197094fc7df279ab203479c3a7
canalun <i.am.kanaru.sato@gmail.com>
2024-02-04 16:40:24 +0900

    lint

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,33 +1,33 @@
 function IndexPopup() {
-    return (
-      <div style={{ padding: "16px" }}>
-        <h2>let's play a game!</h2>
-        <button onClick={sendMessageToIsolatedWorldOnActiveTab}>start!!</button>
-        <button onClick={sendMessageToIsolatedWorldOnActiveTabForTest}>
-          debug mode
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div style={{ padding: "16px" }}>
+      <h2>let's play a game!</h2>
+      <button onClick={sendMessageToIsolatedWorldOnActiveTab}>start!!</button>
+      <button onClick={sendMessageToIsolatedWorldOnActiveTabForTest}>
+        debug mode
+      </button>
+    </div>
+  )
+}
 
-  export default IndexPopup
+export default IndexPopup
 
-  function sendMessageToIsolatedWorldOnActiveTab() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      var activeTab = tabs[0]
-      if (!activeTab.id) {
-        return
-      }
-      chrome.tabs.sendMessage(activeTab.id, { message: "start" })
-    })
-  }
+function sendMessageToIsolatedWorldOnActiveTab() {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var activeTab = tabs[0]
+    if (!activeTab.id) {
+      return
+    }
+    chrome.tabs.sendMessage(activeTab.id, { message: "start" })
+  })
+}
 
-  function sendMessageToIsolatedWorldOnActiveTabForTest() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      var activeTab = tabs[0]
-      if (!activeTab.id) {
-        return
-      }
-      chrome.tabs.sendMessage(activeTab.id, { message: "test" })
-    })
-  }
+function sendMessageToIsolatedWorldOnActiveTabForTest() {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var activeTab = tabs[0]
+    if (!activeTab.id) {
+      return
+    }
+    chrome.tabs.sendMessage(activeTab.id, { message: "test" })
+  })
+}
