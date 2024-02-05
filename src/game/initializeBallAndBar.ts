ad666f8d9eec8229df8ba3ea92e4da4f8d1ed753
canalun <i.am.kanaru.sato@gmail.com>
2024-02-06 03:10:59 +0900

    remove bottomRoom

--- src/game/initializeBallAndBar.ts
+++ src/game/initializeBallAndBar.ts
@@ -1,21 +1,7 @@
 import { initializeBall } from "./ball"
 import { initializeBar } from "./bar"
-import { ballSetting, barSetting, initialBottom } from "./settings"
 
 export function initializeBallAndBar() {
-  const bottomRoom = document.createElement("div")
-  document.documentElement.appendChild(bottomRoom)
-  Object.assign(bottomRoom.style, {
-    position: "absolute",
-    width: "100%",
-    height: `${
-      initialBottom +
-      barSetting.height +
-      ballSetting.height +
-      initialBottom * 1.5
-    }px`
-  })
-
   const ball = initializeBall()
   const table = initializeBar()
 
