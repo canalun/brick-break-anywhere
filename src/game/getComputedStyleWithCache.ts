d6e2dfb3ba67bc3775ea4f8d944defb5b420f12a
canalun <i.am.kanaru.sato@gmail.com>
2024-02-17 14:45:47 +0900

    mod comment

--- src/game/getComputedStyleWithCache.ts
+++ src/game/getComputedStyleWithCache.ts
@@ -1,6 +1,7 @@
 // getComputedStyle() is expensive, so we should cache the result.
-// We can assume that the style of an element doesn't change, except for DOMRect-related,
-// because the page is frozen.
+// We can assume that the style of an element doesn't change, because the page is frozen.
+// Please note that DOMRect-related settings can't be used in this function,
+// because they can change by removing blocks. Use getBoundingClientRect() instead.
 const styleCache = new Map<Element, CSSStyleDeclaration>()
 export function getComputedStyleWithCache(
   element: Element
