3aad11f1f64cb45f3fb7484bde5ad944b15cce5e
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2024-07-23 15:36:25 +0900

    Rename the argument as the function name
    Co-authored-by: canalun <i.am.kanaru.sato@gmail.com>

--- src/game/animation/updateObject.ts
+++ src/game/animation/updateObject.ts
@@ -1,5 +1,5 @@
-export function updateObjectPositionTo(ball: HTMLDivElement, position: { x: number, y: number }): void {
-  Object.assign(ball.style, {
+export function updateObjectPositionTo(obj: HTMLDivElement, position: { x: number, y: number }): void {
+  Object.assign(obj.style, {
     transform:
     `translate(` +
       `${position.x}px, ` +
